7d42b525e17a1faf93867c28bc31eba3a946dae7
Juan <juan@workstation1.localdomain>
2019-10-03 21:54:11 -0500

    Add execise from small section

---
+++ small_exercise/HELLO_YOU.py
@@ -0,0 +1,2 @@
+name=name.upper()
+print(f'\"HELLO {name}\" that is a very nice name with {len(name)} letters.')
---
+++ small_exercise/madlib.py
@@ -0,0 +1,5 @@
+print(f'Please fill in the blanks')
+print(f'__(name)__\'s favorite subject in school is __(subject)__')
+name=input(f'What is the name? ')
+subject=input("Which subject? ")
+print(f'{name}\'s favorite subject in school is {subject}')
