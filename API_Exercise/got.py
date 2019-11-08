80bcbebfec86a2d05830b466f1da548cf823d318
unknown <thebiochemguy@gmail.com>
2019-11-07 21:25:30 -0600

    Code working

--- API_Exercise/got.py
+++ API_Exercise/got.py
@@ -6,17 +6,16 @@ charfileName="char_got.json"
 housefileName="houses_got.json"
 
 def got_url_name():
-    # i=0
-    page=1
-    last_page=43
+    last_page=44
     pageSize=50
 
     data=[]
-    for page in range(0,last_page):
+    for page in range(1,last_page):
         url=f'https://www.anapioficeandfire.com/api/characters?page={page}&pageSize={pageSize}'
         response=requests.get(url)
         results=response.json()
-        data.append(results)
+        for result in results:
+            data.append(result)
 
         
     with open(charfileName, 'w',encoding='utf-8') as f:
@@ -28,16 +27,16 @@ def read_file(fileName):
         return data
 
 def got_url_houses():
-    page=1
-    last_page=9
+    last_page=10
     pageSize=50
 
     data=[]
-    for page in range(0,last_page):
+    for page in range(1,last_page):
         url=f'https://www.anapioficeandfire.com/api/houses?page={page}&pageSize={pageSize}'
         response=requests.get(url)
         results=response.json()
-        data.append(results)
+        for result in results:
+            data.append(result)
 
         
     with open(housefileName, 'w',encoding='utf-8') as f:
@@ -46,26 +45,23 @@ def got_url_houses():
 def getCharHouses(homeurls,houses):
     data=[]
     for home in homeurls:
-        for house in houses:
-            for h in house:
-                if (h["url"] == home):
-                    data.append(h["name"])
+        #Need to remove one to take into account that arrays start at 0
+        id=int(home.split("/")[-1]) - 1 
+        data.append(houses[id]["name"])
+
     return data
 
 
-#    https://anapioficeandfire.com/api/houses/378
 
+
+#These are here for when i want to get the data off of the internet again.
 # got_url_name()
 # got_url_houses()
 
-i=0
-    
 characters=read_file(charfileName)
 houses=read_file(housefileName)
 
 for c in characters:
-     for x in c:
-        i+=1
-        houseList=x["allegiances"]
-        allegences=getCharHouses(houseList,houses)
-        print(f'name:{x["name"]} homes: {allegences}')
+    houseList=c["allegiances"]
+    allegences=getCharHouses(houseList,houses)
+    print(f'name:{c["name"]} homes: {allegences}')
